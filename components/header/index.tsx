cb5bfdcc21e8641372896104e47e31196a7faf24
Jaminhood <princeobj5@gmail.com>
2023-08-28 16:30:27 -0700

    fix: added play [AP-1]

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -4,6 +4,7 @@ import HeaderContent from "./HeaderContent"
 import MobileContent from "./MobileContent"
 
 const Header = () => {
+ const play = []
  return (
   <header className="fixed w-full top-0 z-50 h-20 flex items-center border-b border-gray-300 bg-white shadow-md">
    <Container>
