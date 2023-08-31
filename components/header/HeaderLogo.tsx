4319c9cfa6ccc628fd81d74a086606e05b87eae2
Nzekea Bethel <nzekeabethel@outlook.com>
2023-08-31 21:57:17 +0100

    feat: made the [logo] more distinguished

--- components/header/HeaderLogo.tsx
+++ components/header/HeaderLogo.tsx
@@ -1,5 +1,14 @@
-const HeaderLogo = () => {
- return <div>Logo.</div>
-}
+import Link from 'next/link';
 
-export default HeaderLogo
+const HeaderLogo = () => {
+	return (
+		<Link
+			href={'/'}
+			className='px-8 text-2xl font-bold transition-colors duration-500 delay-100 bg-gray-200 border-2 rounded-full hover:bg-slate-900 hover:text-gray-100'
+		>
+			Logo.
+		</Link>
+	);
+};
+
+export default HeaderLogo;
